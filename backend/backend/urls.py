a034ed140120d92da11256027890c0c78b708f9b
MerVitz <amakaluvitalis202@gmail.com>
2024-05-30 00:17:08 +0300

    set the API endpoints successfully, from the backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from backend_app import views
+
+router = DefaultRouter()
+router.register(r'reviews', views.ReviewViewSet)
+router.register(r'comments', views.CommentViewSet)
+router.register(r'maintenances', views.MaintenanceViewSet)
+router.register(r'notifications', views.NotificationViewSet)
+router.register(r'payments', views.PaymentViewSet)
+router.register(r'bookings', views.BookingViewSet)
+router.register(r'equipment', views.EquipmentViewSet)
+router.register(r'users', views.CustomUserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- backend/backend_app/views.py
+++ backend/backend_app/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Review, Comment, Maintenance, Notification, Payment, Booking, Equipment,CustomUser
+from .serializers import ReviewSerializer,CustomUserSerializer,EquipmentSerializer,BookingSerializer, PaymentSerializer,NotificationSerializer,MaintenanceSerializer,CommentSerializer
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class MaintenanceViewSet(viewsets.ModelViewSet):
+    queryset = Maintenance.objects.all()
+    serializer_class = MaintenanceSerializer
+
+class NotificationViewSet(viewsets.ModelViewSet):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+
+class PaymentViewSet(viewsets.ModelViewSet):
+    queryset = Payment.objects.all()
+    serializer_class = PaymentSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+class EquipmentViewSet(viewsets.ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
+
+class CustomUserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+
+
 
 # Create your views here.
