7b230c415694291518c63a61659ccc74f9f2906e
MerVitz <amakaluvitalis202@gmail.com>
2024-09-13 01:07:20 +0300

    modify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/FlexiHire/frontend',
+  base: '/FlexiHire/',
   plugins: [react()],
   build: {
     rollupOptions : {
