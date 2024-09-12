9cb1b3f524e4b0e02939825c05f9eb7bd74abe31
MerVitz <amakaluvitalis202@gmail.com>
2024-09-13 00:59:23 +0300

    Updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/FlexiHire/',
   plugins: [react()],
   build: {
     rollupOptions : {
