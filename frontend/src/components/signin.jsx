93bc9365d1fe4280fbb60cc73ace8405d5799ac2
MerVitz <amakaluvitalis202@gmail.com>
2024-09-19 14:54:30 +0300

    fixed the session issue
    token, user id and user_type are stored to be used in sessions.

--- frontend/src/components/signin.jsx
+++ frontend/src/components/signin.jsx
@@ -8,27 +8,39 @@ import './styles/signin.css';
 function SignIn() {
   const [email, setEmail] = useState('');``
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
   const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Accessing th login endpoint
       const response = await axios.post('http://localhost:8000/api/login/', { email, password });
+
+      //Storing the users detials , used for a given session
       localStorage.setItem('token', response.data.token);
       localStorage.setItem('user_id', response.data.user_id);
       localStorage.setItem('user_type', response.data.user_type);
+
+      //Loggin gusers details for debugging.
       console.log('session user', response.data.user_id)
       console.log('Session token', response.data.token)
       console.log('session user type', response.data.user_type)
-      if (response.data.user_type === 'admin') {
-        navigate('/admin-dashboard');
-      } else if (response.data.user_type === 'customer') {
+
+      // Redirect to home page if the user_type is 'admin' or 'customer'
+      if (response.data.user_type === 'admin' || response.data.user_type === 'customer') {
         navigate('/home');
       } else {
-        navigate('/');
+        // If user_type is invalid, clear the session and show an error message
+        localStorage.removeItem('token');
+        localStorage.removeItem('user_id');
+        localStorage.removeItem('user_type');
+        setErrorMessage('There was a technical error. Please contact support.');
       }
     } catch (error) {
       console.error('There was an error logging in!', error);
+      setErrorMessage('Invalid email or password. Please try again.');
     }
   };
 
@@ -36,6 +48,7 @@ function SignIn() {
     <div className="signin-container">
       <div className="signin-form">
         <h2>Log in to your account</h2>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
         <p>Don&#39;t have an account? <a href="/signup">Sign Up</a></p>
         <button className="social-login google">Google</button>
         <button className="social-login github">GitHub</button>
